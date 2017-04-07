e0e167fad4bf9accc1bd041e39c5a4e72309e2b9
Devon <Sanitygamers@outlook.com>
2017-04-07 10:01:58 -0500

    Update magic.java

--- Assignment2.8/src/magic.java
+++ Assignment2.8/src/magic.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 
 public class magic {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Scanner keyboard = new Scanner(System.in);
-		int input = keyboard.nextInt();
-		String input1 = "input";
-		char firstLetter = input1.charAt(0);
-		char secondLetter = input1.charAt(1);
-		char thirdLetter = input1.charAt(2);
-		char fourthLetter = input1.charAt(3);
-		char fifthLetter = input1.charAt(4);
+		
+		String input = keyboard.nextLine();
+		
+		char[] letters = new char[input.length()];
+		
+		String TotalPrint = "";
 		
 		System.out.print("Enter the word: ");
+		
 		System.out.println(input);
-		System.out.println(firstLetter);
-		System.out.println(secondLetter);
-		System.out.println(thirdLetter);
-		System.out.println(fourthLetter);
-		System.out.println(fifthLetter);
+		
+		for(int x = 0; x < input.length(); x++) {
+			letters[x] = input.charAt(x);
+			TotalPrint = TotalPrint + letters[x];
+			System.out.println(TotalPrint);
+		}
 	}
-
 }
