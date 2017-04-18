fca1ac4112c011ef31bc3f7e974623564a74ecd5
Devon <Sanitygamers@outlook.com>
2017-04-18 14:53:00 -0500

    Update numberGuess.java

--- Daniel_Yi_Eclipse/Assignment 3/src/numberGuess.java
+++ Daniel_Yi_Eclipse/Assignment 3/src/numberGuess.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
+
+import java.util.Random;
+
 public class numberGuess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Random r1 = new Random();
 
 		Scanner keyboard= new Scanner (System.in);
 		int number1 = 97;
-		
+		int randNumb = r1.nextInt(100);
 		int odd;
 		int even;
 		int hintOne = 51;
 		//int hintTwo = even;
 		int five = 5;
-		boolean 9 = 5;
+		//boolean 9 = 5; makes no sense
 		
 		
 	}
