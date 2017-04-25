b9590d0c315bf3444a94cede4ea1022f98c84959
Devon <Sanitygamers@outlook.com>
2017-04-25 12:45:15 -0500

    Simplified code, also cleaned

--- Daniel_Yi_Eclipse/Assignment 3/src/numberGuess.java
+++ Daniel_Yi_Eclipse/Assignment 3/src/numberGuess.java
@@ -4,38 +4,18 @@ import java.util.Random;
 public class numberGuess {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-
+		
 		Scanner keyboard= new Scanner (System.in);
 		Random r1 = new Random();
 		
-		//int input = r1.nextInt(101);
-		//int d1 = r1;
-		//int question1 = 50;
 		int firstDigit = r1.nextInt(101);
 		
+		boolean question1 = (firstDigit>50);
+		boolean question2 = (firstDigit%5==0 && (firstDigit%10!=0) || (firstDigit/10>4.9) || (firstDigit/10<6));
+		boolean question3;
+		
 		System.out.print(firstDigit);
-		
-		
-		if(firstDigit%5==0 && !(firstDigit%10==0) || (firstDigit==50)) {
-			System.out.println("Number has a five: " + true);
-		}// else if(firstDigit%5==0 && !(firstDigit%10==0)) {
-			//System.out.print("Number has a five: " + true);
-		//} else !((firstDigit%5==0) || (firstDigit%10==0)) {
-		
-	     //}
-		
-		
-		
-		
-		//boolean finalFirstAnswer = answer1 && answer2; 
-		//int nothing = 0;
-		 
-		
-		
-		
-		
+		System.out.println("Number greater than 50? " + question1);
+	        System.out.println("Number has a five: " + question2);					
 	}
-
 }
