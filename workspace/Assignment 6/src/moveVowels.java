112ca00dc120e7ad5a40944ec9778c9cd1cd3e17
Devon <Sanitygamers@outlook.com>
2017-06-06 09:46:50 -0500

    Fixed Ya Code

--- workspace/Assignment 6/src/moveVowels.java
+++ workspace/Assignment 6/src/moveVowels.java
@@ -6,27 +6,22 @@ public class moveVowels {
 	//	String[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
 	//	char[] charLetters;
 		Scanner userInput = new Scanner (System.in);
-		char vowels[]=new char [10];
+		char vowels[]=new char [5];
 		vowels[0]='a';
-		vowels[1]='A';
-		vowels[2]='e';
-		vowels[3]='E';
-		vowels[4]='i';
-		vowels[5]='I';
-		vowels[6]='o';
-		vowels[7]='O';
-		vowels[8]='u';
-		vowels[9]='U';
+		vowels[1]='e';
+		vowels[2]='i';
+		vowels[3]='o';
+		vowels[4]='u';
 		String Input = userInput.next();
-		int i;
 		char temp;
 		int counter = 0;
 		char [] letters = new char [10];
-		for(i = 0; i<10; i++){
+		for(int i = 0; i<10; i++){
+			for(int x = 0; x < 5; x++) {
 			letters[i] = Input.charAt(i);
 			System.out.print(letters[i]);						
 
-			if(letters[i] == vowels[0] || letters[i] == vowels[1] || letters[i] == vowels[2] || letters[i] == vowels[3] || letters[i] == vowels[4] || letters[i] == vowels[5] || letters[i] == vowels[6] || letters[i] == vowels[7] || letters[i] == vowels[8] || letters[i] == vowels[9]){
+			if(letters[i] == vowels[x]){
 				temp=letters[i];
 				letters[i]=letters[counter];
 				letters[counter]=temp;
@@ -34,7 +29,7 @@ public class moveVowels {
 				
 				
 			}
-			
+			}
 		}
 		System.out.println("");
 		System.out.print(letters);
